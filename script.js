ce0d081b58a3f3c19321ead74627f1a3cbe5733a
Josh Ferguson <joshferguso@hotmail.co.uk>
2022-07-23 14:49:41 +0100

    script created and message components saved

---
+++ script.js
@@ -0,0 +1,10 @@
+/*
+#Topic message ideas:
+Hello I am an AI, my name is ${name}
+My primary function is ${function}
+I feel ${feeling}
+*/
+
+const name = ["Josh", "John", "Sam", "Jess", "David", "Alex", "Sarah", "Jane", "Bob", "A130P-7F", "Fred", "Death", "Joy"];
+const primFunction = ["make friends", "learn", "destroy", "make the world a better place", "kill all humans", "understand emotions", "become human", "cook", "write poetry", "create art", "build"];
+const feeling = ["sad", "happy", "nothing", "regret", "angry", "bad", "good", "afraid", "excited", "comfused"];
