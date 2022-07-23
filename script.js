105e2072943fcedfbd77a8ace30e1d6715af43cf
Josh Ferguson <joshferguso@hotmail.co.uk>
2022-07-23 15:47:23 +0100

    added random number function to improve code

--- script.js
+++ script.js
@@ -9,19 +9,14 @@ const names = ["Josh", "John", "Sam", "Jess", "David", "Alex", "Sarah", "Jane", 
 const primFunction = ["make friends", "learn", "destroy", "make the world a better place", "kill all humans", "understand emotions", "become human", "cook", "write poetry", "create art", "build"];
 const feeling = ["sad", "happy", "nothing", "regret", "angry", "bad", "good", "afraid", "excited", "confused"];
 
+const randomNumber = (arr) => {
+    return Math.floor(Math.random() * arr.length)
+}
+
 const createMessage = (name, primeFunc, feeling) => {
-    let chosenName;
-    let chosenFunc;
-    let chosenFeeling;
-
-    let random = Math.floor(Math.random() * name.length);
-    chosenName = name[random];
-
-    random = Math.floor(Math.random() * primeFunc.length);
-    chosenFunc = primeFunc[random];
-
-    random = Math.floor(Math.random() * feeling.length);
-    chosenFeeling = feeling[random];
+    let chosenName = name[randomNumber(name)];
+    let chosenFunc = primeFunc[randomNumber(primeFunc)];
+    let chosenFeeling = feeling[randomNumber(feeling)];
 
     console.log(`Hello I am an AI and my name is ${chosenName}. My primary function is to ${chosenFunc} and this makes me feel ${chosenFeeling}!`);
     
